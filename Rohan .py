3964fa7f40e79d87805e3d19dba41d249ed2a614
7382022 <amreenrida2@gmail.com>
2024-09-28 15:32:21 -0400

    Rohan.py

---
+++ Rohan .py
@@ -0,0 +1,11 @@
+weather = input("Please enter the weather: ")
+
+if weather == "cold":
+    print("The weather is cold, then Rohan will wear a jacket.")
+elif weather == "mid":
+    print("The weather is mild, Rohan will wear a t-shirt and pants.")
+elif weather == "hot":
+    print("The weather is hot, then Rohan will wear a shirt and shorts.")
+else:
+    print("The weather is boiling, then Rohan will go to the beach.")
+    
